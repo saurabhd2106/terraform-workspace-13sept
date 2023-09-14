add9ccac32bf5db4cabb2f5e0ecb07fbeb671914
= <=>
2023-09-14 12:59:19 +0100

    remove secrest

--- resource-group/provider.tf
+++ resource-group/provider.tf
@@ -14,9 +14,6 @@ provider "azurerm" {
 
   }
 
-  client_id       = "b6132866-56ea-4f9f-91f5-16af746ac288"
-  client_secret   = "eUy8Q~EFM2_ds2Um3Z6x.TZyDOua~lSvMf1qDc_t"
-  tenant_id       = "fa1e26bd-a406-4570-a09c-e0f3083223c6"
-  subscription_id = "e0b47bea-67c1-44d3-a3ae-a494d59ab222"
+ 
 }
 
